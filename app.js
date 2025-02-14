30944543dfd52add28b2a94837b51a23ffd3a9eb
ItsAiz <andres.sierra03@uptc.edu.co>
2025-02-13 20:28:37 -0500

    Add introspection handling by environment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 const startApolloServer = async () => {
     const apolloServer = new ApolloServer({
-        introspection: true,
+        introspection: process.env.NODE_ENV !== 'PROD',
         typeDefs: mergeTypeDefs(schemas),
         resolvers,
     });
